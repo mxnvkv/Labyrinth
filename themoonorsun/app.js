4fccf02c831769864e7b86d1d990873c3667471f
Zakkaray <bloomzakk@gmail.com>
2019-02-02 16:58:55 +0200

    Initial commit

---
+++ themoonorsun/app.js
@@ -0,0 +1,20 @@
+const first = document.getElementById('first');
+const second = document.getElementById('second');
+const third = document.getElementById('third');
+const forth = document.getElementById('forth');
+
+first.addEventListener("mouseover", function () {
+      document.body.style.backgroundColor = "#212123";
+});
+
+second.addEventListener("mouseover", function () {
+      document.body.style.backgroundColor = "#656565";
+});
+
+third.addEventListener("mouseover", function () {
+      document.body.style.backgroundColor = "rgb(45, 56, 64)";
+});
+
+forth.addEventListener("mouseover", function () {
+      document.body.style.backgroundColor = "#656565";
+});
